af3664fbc7ef4f7eed64fd26cacc10fd13d6feee
kasaranenikarthikeya <kasaranenikarthikeya4@gmail.com>
2026-05-25 20:43:38 +0530

    added env example

--- main.py
+++ main.py
@@ -127,8 +127,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:3001",
-        "https://chitchat-client-nato.onrender.com",
-        "https://chitchat-f4e6.onrender.com"
+        "https://agent-6a145e02890e234f81c19d4d--chitchat-clientd.netlify.app/",
+        "http://13.48.46.222"
     ] + CORS_ORIGINS,
     allow_origin_regex=r"https://.*\.netlify\.app",
     allow_credentials=True,
