dc6642b7ffb7104f6d138dc1806443f77787e4ea
kasaranenikarthikeya <kasaranenikarthikeya4@gmail.com>
2026-05-25 22:39:31 +0530

    added env example

--- main.py
+++ main.py
@@ -124,12 +124,7 @@ app = FastAPI(lifespan=lifespan)
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://chitchat-clientd.netlify.app",
-        "https://13.48.46.222.nip.io",
-        "http://localhost:3000",
-        "http://localhost:3001",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
